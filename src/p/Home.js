5dcefd11fb2a1099f121738f66f8207b117ab1c8
nik frank <nikbfrank@gmail.com>
2017-08-06 19:56:17 +0300

    responsive styles for Home screen

--- src/p/Home.js
+++ src/p/Home.js
@@ -2,123 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 import {GridList, GridTile} from 'material-ui/GridList';
 import Subheader from 'material-ui/Subheader';
-import FaCompass from 'react-icons/lib/fa/compass';
-import FaPlayCircleO from 'react-icons/lib/fa/play-circle-o';
 import PropTypes from 'prop-types';
 
+import tiles from './homeTiles';
 
-import mishna from '../imgs/mishna.png';
-const imgs = {
-  mishna,
-};
+import bannerImg from '../imgs/main-20yr.png';
 
-const homeGridStyles = {
-  root: {
-    display: 'flex',
-    position: 'absolute',
-    justifyContent: 'space-around',
-    minWidth: '20vw',
-    marginLeft: '20%',
-    flexFlow: 'row rap',
-  },
-  gridList: {
-    overflowY: 'auto',
-  },
-};
-
-const tile = [
-  {
-    img: imgs.mishna,
-    title: 'Mishna',
-  },
-  {
-    img: 'images/grid-list/burger-827309_640.jpg',
-    title: 'Insights',
-  },
-  {
-    img: 'images/grid-list/camera-813814_640.jpg',
-    title: 'Background',
-  },
-  {
-    img: 'images/grid-list/morning-819362_640.jpg',
-    title: 'Review Q&A',
-  },
-  {
-    img: 'images/grid-list/hats-829509_640.jpg',
-    title: 'Full Review',
-  },
-  {
-    img: 'images/grid-list/honey-823614_640.jpg',
-    title: 'English Charts',
-  },
-  {
-    img: 'images/grid-list/vegetables-790022_640.jpg',
-    title: 'Point by Point',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: 'Halachah',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: 'Daily Quiz',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: 'Tosfos',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: "Revach L'daf",
-  },
-  {
-    Icon: FaPlayCircleO,
-    title: 'Podcasts',
-  },
-  {
-    Icon: FaCompass,
-    title: 'Navigator',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: 'יוסף דעת',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: 'טבלאות',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: 'גלי מסכתא',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: 'חידונים',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: 'Yerushalmi matchup',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: 'Yerushalmi Audio',
-  },
-  {
-    img: 'images/grid-list/water-plant-821293_640.jpg',
-    title: 'Our Replies',
-  },
-];
 
 class Home extends Component {
   render() {
+
+    const pageWidth = (this.context.width < 800) ?
+                      'calc( 100vw - 30px )' :
+                      'calc( 100vw - 300px )';
+          
+    const homeGridStyles = {
+      root: {
+        display: 'flex',
+        position: 'absolute',
+        justifyContent: 'space-around',
+        width: pageWidth,
+        marginLeft: (this.context.width < 800) ? 10 :
+                    (this.context.width < 900) ? 286 :
+                    280,
+        
+        flexFlow: 'row rap',
+      },
+      gridList: {
+        overflowY: 'auto',
+      },
+    };
+
+    
     return (
       <div>
+        <img src={bannerImg} style={{ width: 'auto',
+                                      height: '200px',
+                                      display: 'block',
+                                      margin: '0 auto'}}/>
         <div style={homeGridStyles.root}>
+          
+
           <GridList
               cellHeight={180}
               style={homeGridStyles.gridList}
               className='tileList'
-              cols={this.context.width > 768 ? 5 : 2}>
+              cols={this.context.width > 900 ? 5 : 3}>
+
             <Subheader>
               <div className='homeGridHeader'>
                 /*THE CURRENT SEDER NEZIKIN HAS BEEN DEDICATED לע"נ
@@ -135,8 +65,9 @@ class Home extends Component {
                 Chesed and Tzedakah. He will long be remembered.*/
               </div>
             </Subheader>
+
             {
-              tile.map(({ title, img, Icon }, i) => (
+              tiles.map(({ title, img, Icon }, i) => (
                 <a className='tileA' href="#/about" key={i}>
                   <GridTile
                       title={title}
