4bf40fe8a03d8e36a1ba0523cb151ff92e83070b
nik frank <nikbfrank@gmail.com>
2017-08-23 14:52:57 +0300

    hebrew table switchy

--- src/p/Insights.js
+++ src/p/Insights.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react';
+import './Insights.css';
+
+import hebTabGif from '../imgs/heb-tab.gif';
+import hebrewNumbers from '../hebrew-numbers';
 
 const pageCopy = {
-  en: 'blah blah blah',
-  he: 'בלה בלה בלה',
+  en: {
+    header: 'OUTLINES OF HALACHOS FROM THE DAF',
+    p: `Summary, in outline form, of how a Halachic ruling unfolded from the Daf
+based on the rulings of Rishonim, Achronim and modern-day Poskim`,
+    daf: 'Daf ',
+  },
+  he: {
+    header: 'נקודות סיכום הלכה מדף',
+    p: `סיכום, בצורה מתוארת, כיצד הלכה הלכה מן הדף
+על פי פסקי ראשונים, אחרונים ופוסקים מודרניים`,
+    daf: 'דף ',
+  }
 };
 
 class Insights extends Component {
@@ -22,8 +36,48 @@ class Insights extends Component {
   render() {
     return (
       <div className="InsightsPage">
-        <button onClick={this.toggleLang}>Toggle</button>
-        <p> { pageCopy[this.state.lang] } </p>
+        <button style={{ float: 'right' }} onClick={this.toggleLang}>Toggle Language</button>
+
+        <img src={hebTabGif}/>
+        
+        <h3>{pageCopy[this.state.lang].header}</h3>
+        <p>
+          {
+            pageCopy[this.state.lang].p.split('\n').map((pp, pi)=> (
+              <span key={pi}>
+                <span>{pp}</span>
+                <br/>
+              </span>
+            ))
+          }
+        </p>
+
+        <table>
+          <tbody>
+            {
+              Array(15).fill(1).map((o, i)=> (
+                <tr key={i}>
+                  {
+                    Array(7).fill(1).map((p, j)=> ( (i*7 + j + 1 > 103)? null :
+                      <td key={j}>
+                        <a href="#/blah">
+                          {pageCopy[this.state.lang].daf}
+                          {
+                            this.state.lang === 'en'? (
+                              i*7 + j + 1
+                            ) : (
+                              hebrewNumbers[i*7 + j + 1]
+                            )
+                          }
+                        </a>
+                      </td>
+                    ) )
+                  }
+                </tr>
+              ) )
+            }
+          </tbody>
+        </table>
       </div>
     );
   }
